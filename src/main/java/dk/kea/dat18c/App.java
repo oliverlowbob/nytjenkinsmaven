1e6d73226a052a24a10322487dcac5f6cc4d952b
oliverlowbob <oliv605m@stud.kea.dk>
2019-10-10 11:38:24 +0200

    first commit

---
+++ src/main/java/dk/kea/dat18c/App.java
@@ -0,0 +1,13 @@
+package dk.kea.dat18c;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
